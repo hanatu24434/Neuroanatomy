dab9557a9542989078809c7968ff3457d1320424
hanatu24434 <hannatuaforolagba@gmail.com>
2025-08-13 16:08:58 +0100

    add work

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 function menu() {
-    var x=document.getElementById('links');
-    if (x.style.difusplay === "block") {
+    var x=document.getElementById("links");
+    if (x.style.display === "block") {
         x.style.display = "none";
     } else {
         x.style.display = "block";
