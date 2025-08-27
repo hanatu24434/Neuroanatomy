da5854ec0097146121ee4d136e7956493175c354
hanatu24434 <hannatuaforolagba@gmail.com>
2025-08-27 16:09:40 +0100

    change

--- myscript.js
+++ myscript.js
@@ -1,5 +1,5 @@
 function menu() {
-    var x=document.getElementById("links");
+    var x = document.getElementById("links");
     if (x.style.display === "block") {
         x.style.display = "none";
     } else {
